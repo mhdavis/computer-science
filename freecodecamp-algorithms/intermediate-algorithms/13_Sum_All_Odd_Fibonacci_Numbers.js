07fef882ce915df18b3ab9eb92af07b071b1034f
Michael Davis <mhdavis1993@gmail.com>
2017-11-29 17:59:25 -0500

    completed sum all odd fibonacci nums

---
+++ freecodecamp-algorithms/intermediate-algorithms/13_Sum_All_Odd_Fibonacci_Numbers.js
@@ -0,0 +1,37 @@
+function sumFibs(num) {
+  var fib = fibonacci(num);
+
+  var total = 0;
+  // add only odd values of fibonacci
+  // array to total
+  for (var i=0; i < fib.length; i++) {
+    if (fib[i] % 2 !== 0 && fib[i] <= num) {
+      total += fib[i];
+    }
+  }
+
+  return total;
+}
+
+// helper function that generates
+// an array with num terms of the
+// fibonacci sequence
+function fibonacci(num) {
+  var arr = [];
+  var a = 1, b = 0, temp;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    arr.push(b);
+    num--;
+  }
+
+  return arr;
+}
+
+console.log(sumFibs(4));
+// => 5
+console.log(sumFibs(75025));
+// => 125721
