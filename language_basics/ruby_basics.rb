fad8a0676ab4108099e5084d384b2c8ef8c204e1
Michael Davis <mdavis@C02J6292DKQ4.local>
2018-02-21 14:02:42 -0500

    Added language basics dir
    - Began adding ruby basics

---
+++ language_basics/ruby_basics.rb
@@ -0,0 +1,36 @@
+# ruby is interpreted - no compiler needed!
+
+# this is how you print a string
+puts "hello world"
+
+# this is how you write variables in ruby
+my_num = 5
+my_string = "Ruby"
+puts my_num
+puts my_string
+
+# arithmetic in Ruby
+puts 3 + 3 # addition
+puts 3 - 3 # subtraction
+puts 3 * 3 # multiplication
+puts 3 / 3 # division
+puts 3**3  # exponents
+puts 5%3   # remainder division
+
+# puts vs print
+puts "What's up?" # adds \n to end of string
+print "Oxnard" # doesn't add \n
+
+# string methods
+puts practice_string = "Check it out"
+puts practice_string.length
+puts practice_string.reverse
+puts practice_string.upcase
+puts practice_string.downcase
+
+# multiline comments
+=begins
+All of this stuff
+is hidden
+from you
+=end
