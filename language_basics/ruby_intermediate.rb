792b36bd4e03395a92af97785a2cee26c81462c2
Michael Davis <mhdavis1993@gmail.com>
2018-02-23 08:17:31 -0500

    ruby hashes, hash methods

--- language_basics/ruby_intermediate.rb
+++ language_basics/ruby_intermediate.rb
@@ -260,3 +260,47 @@ rejected = array.reject{|num| num > 1}
 
 # array.reduce
 reduced = array.reduce{|sum, x| sum + x} # => 6
+
+# ---------------- HASHS -------------------
+# hash is a collection of key/object pairs
+number_hash = {
+  "PI" => 3.14,
+  "Golden" => 1.618,
+  "e"=> 2.718
+}
+
+puts number_hash["PI"]
+
+# other way of generating a hash
+superheroes = Hash["Clark Kent", "Superman", "Bruce Wayne", "Batman"]
+puts superheroes["Clark Kent"] # returns Superman
+
+# adding to a hash
+superheroes["Barry Allen"] = "Flash"
+puts superheroes["Barry Allen"]
+
+# Set a default key value
+samp_hash = Hash.new("No Such Key")
+puts samp_hash["Dog"]
+
+# combine 2 hashes in a destructive way with update
+# or with no overwritting via merge
+superheroines = Hash["Lisa Morel", "Aquagirl", "Diana Prince", "Wonder Woman"]
+superheroes.update(superheroines)
+
+# Print out each key and value
+superheroes.each do |key, value|
+  puts "#{key.to_s} : #{value}"
+end
+
+# ---------------- HASH METHODS -------------------
+# hash.has_key?
+puts "Has Key Lisa Morel : " + superheroes.has_key?("Lisa Morel").to_s
+# hash.has_value?
+puts "Has Value Batman : " + superheroes.has_value?("Batman").to_s
+# hash.empty?
+puts "Is Hash Empty : " + superheroes.empty?.to_s
+# hash.size?
+puts "Size of Hash : " + superheroes.size.to_s
+#hash.delete
+superheroes.delete("Barry Allen")
