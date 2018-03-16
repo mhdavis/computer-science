fb97caa9226fccabcdeee335bd4d2d29d2cee493
Michael Davis <mhdavis1993@gmail.com>
2018-03-16 12:55:39 -0400

    converted conditionals examples into class

--- language_basics/groovy/groovy_basics.groovy
+++ language_basics/groovy/groovy_basics.groovy
@@ -346,40 +346,42 @@ class RangeExample {
 }
 
 // ---------------- CONDITIONALS -------------------
-  // Conditonal Operators : ==, !=, >, <, >=, <=
+// Conditonal Operators : ==, !=, >, <, >=, <=
+// Logical Operators : && || !
+class ConditionalExample {
+  static void main(String[] args) {
+    def ageOld = 6;
 
-  // Logical Operators : && || !
+    if(ageOld == 5){
+      println("Go to Kindergarten");
+    } else if((ageOld > 5) && (ageOld < 18)) {
+      printf("Go to grade %d \n", (ageOld - 5));
+    } else {
+      println("Go to College");
+    }
 
-  def ageOld = 6;
+    def canVote = true;
 
-  if(ageOld == 5){
-    println("Go to Kindergarten");
-  } else if((ageOld > 5) && (ageOld < 18)) {
-    printf("Go to grade %d \n", (ageOld - 5));
-  } else {
-    println("Go to College");
+    // Ternary operator
+    println(canVote ? "Can Vote" : "Can't Vote");
+
+    // Switch statement
+    switch(ageOld) {
+      case 16: println("You can drive");
+      case 18:
+        println("You can vote");
+
+        // Stops checking the rest if true
+        break;
+      default: println("Have Fun");
+    }
+
+    // Switch with list options
+    switch(ageOld){
+      case 0..6 : println("Toddler"); break;
+      case 7..12 : println("Child"); break;
+      case 13..18 : println("Teenager"); break;
+      default : println("Adult");
+    }
   }
-
-  def canVote = true;
-
-  // Ternary operator
-  println(canVote ? "Can Vote" : "Can't Vote");
-
-  // Switch statement
-  switch(ageOld) {
-    case 16: println("You can drive");
-    case 18:
-      println("You can vote");
-
-      // Stops checking the rest if true
-      break;
-    default: println("Have Fun");
-  }
-
-  // Switch with list options
-  switch(ageOld){
-    case 0..6 : println("Toddler"); break;
-    case 7..12 : println("Child"); break;
-    case 13..18 : println("Teenager"); break;
-    default : println("Adult");
-  }
+}
