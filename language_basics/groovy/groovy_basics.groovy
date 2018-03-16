beb25f2a82c939e9430c227867b7472b3b656abb
Michael Davis <mhdavis1993@gmail.com>
2018-03-16 11:08:10 -0400

    added I/O section

--- language_basics/groovy/groovy_basics.groovy
+++ language_basics/groovy/groovy_basics.groovy
@@ -181,6 +181,37 @@ class StringsExample {
     println("Ant <=> Ant " + ('Ant' <=> 'Ant'));
   }
 }
+// ---------------- OUTPUT -------------------
+class OutputExample {
+  static void main(String[] args) {
+   // With double quotes we can insert variables
+   def randString = "Random";
+   println("A $randString string");
+
+   // You can do the same thing with printf
+   printf("A %s string \n", randString);
+
+   // Use multiple values
+   printf("%-10s %d %.2f %10s \n", ['Stuff', 10, 1.234, 'Random']);
+  }
+}
+
+// ---------------- INPUT -------------------
+class InputExample {
+  static void main(String[] args) {
+    print("Whats your name ");
+    def fName = System.console().readLine();
+    println("Hello " + fName);
+
+    // You must cast to the right value
+    // toInteger, toDouble
+    print("Enter a number ");
+    def num1 = System.console().readLine().toDouble();
+    print("Enter another ");
+    def num2 = System.console().readLine().toDouble();
+    printf("%.2f + %.2f = %.2f \n", [num1, num2, (num1 + num2)]);
+  }
+}
 
 // ---------------- ASSERATIONS -------------------
 /*
