ba09d596109a50b38f803a966ad954f8e65eeb0a
Michael Davis <mhdavis1993@gmail.com>
2018-03-16 12:51:47 -0400

    added section on ranges

--- language_basics/groovy/groovy_basics.groovy
+++ language_basics/groovy/groovy_basics.groovy
@@ -316,3 +316,31 @@ class MapsExample {
     println("Size " + paulMap.size());
   }
 }
+
+// ---------------- RANGES -------------------
+// Ranges represent a range of values in shorthand notation
+class RangeExample {
+  static void main(String[] args) {
+    def oneTo10 = 1..10;
+    def aToZ = 'a'..'z';
+    def zToA = 'z'..'a';
+
+    println(oneTo10);
+    println(aToZ);
+    println(zToA);
+
+    // Get size
+    println("Size " + oneTo10.size());
+
+    // get index
+    println("2nd Item " + oneTo10.get(1));
+
+    // Check if range contains
+    println("Contains 11 " + oneTo10.contains(11));
+
+    // Get last item
+    println("Get Last " + oneTo10.getTo());
+
+    println("Get First " + oneTo10.getFrom());
+  }
+}
