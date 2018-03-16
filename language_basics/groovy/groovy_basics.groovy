81c2e9502d5e000211b1f7add10ee5bcd9f70a61
Michael Davis <mhdavis1993@gmail.com>
2018-03-16 10:37:56 -0400

    restructured groovy basics

--- language_basics/groovy/groovy_basics.groovy
+++ language_basics/groovy/groovy_basics.groovy
@@ -1,11 +1,70 @@
 
 // ---------------- CLASSES -------------------
 /*
-In groovy, everything is a class (including strings and integers)
+First and foremost:
+In groovy, everything is a class
+(including strings and integers).
+This is how you write hello word.
 */
-class HelloController {
-  def world = {
-    render "Hello World it's " new.java.util.Date()
+class HelloWorldExample {
+  // static - accessible method
+  // void - doesn't return a value
+  // args - a String Array (String[])
+  static void main(String[] args) {
+    println("Hello World");
+  }
+}
+
+// ---------------- VARIABLES -------------------
+class VariablesExample {
+  static void main(String[] args) {
+    def age = "Dog";
+    age = 40;
+  }
+}
+
+
+// ---------------- ARITHEMATIC -------------------
+class ArithematicExample {
+  // main is where execution starts
+  static void main(String[] args) {
+    /*
+    Everything in Groovy is an object
+    including numbers
+    */
+
+    // The basic integer math operators
+    println("5 + 4 = " + (5 + 4));
+    println("5 - 4 = " + (5 - 4));
+    println("5 * 4 = " + (5 * 4));
+    println("5 / 4 = " + (5.intdiv(4)));
+    println("5 % 4 = " + (5 % 4));
+
+    // Floating point math operators
+    println("5.2 + 4.4 = " + (5.2.plus(4.4)));
+    println("5.2 - 4.4 = " + (5.2.minus(4.4)));
+    println("5.2 * 4.4 = " + (5.2.multiply(4.4)));
+    println("5.2 / 4.4 = " + (5.2 / 4.4));
+
+    // Order of operations
+    println("3 + 2 * 5 = " + (3 + 2 * 5));
+    println("(3 + 2) * 5 = " + ((3 + 2) * 5));
+
+    // Increment and decrement
+    println("age++ = " + (age++)); // print then increment
+    println("++age = " + (++age)); // increment then print
+    println("age-- = " + (age--)); // print then decrement
+    println("--age = " + (--age)); // decrement then print
+
+    // Largest values
+    println("Biggest Int " + Integer.MAX_VALUE);
+    println("Smallest Int " + Integer.MIN_VALUE);
+
+    println("Biggest Float " + Float.MAX_VALUE);
+    println("Smallest Float " + Float.MIN_VALUE);
+
+    println("Biggest Double " + Double.MAX_VALUE);
+    println("Smallest Double " + Double.MIN_VALUE);
   }
 }
 
@@ -14,77 +73,33 @@ class HelloController {
 Here we simply verify that the variable num has a value
 of 1 at the point of execution in the code
 */
-class AssertionsClass {
-  def num = 1
-  assert num == 1
-}
-
-// ---------------- GROOVY STRINGS -------------------
-/* essentially string interpolation in groovy */
-def person = "John"
-
-println """
-${new Date()}
-
-Dear $person,
-
-This is a Groovy Letter!
-
-Sincerely,
-Bob
-"""
-
-// ---------------- ARITHEMATIC -------------------
-class ArithematicClass {
-  // main is where execution starts
-  static void main(String[] args) {
-    println("Hello World");
-  }
-
+class AssertionsExample {
   /*
-  Everything in Groovy is an object
-  including numbers
-
   def is used when you define a variable
   Variables start with a letter and can
   contain numbers and _
   Variables are cynamically typed and can
   hold any value
   */
-  def age = "Dog";
-  age = 40;
+  def num = 1
+  assert num == 1
+}
 
-  // The basic integer math operators
-  println("5 + 4 = " + (5 + 4));
-  println("5 - 4 = " + (5 - 4));
-  println("5 * 4 = " + (5 * 4));
-  println("5 / 4 = " + (5.intdiv(4)));
-  println("5 % 4 = " + (5 % 4));
+// ---------------- GROOVY STRINGS -------------------
+/* essentially string interpolation in groovy */
+class GStringExample {
+  static void main(String[] args) {
+    def person = "John"
 
-  // Floating point math operators
-  println("5.2 + 4.4 = " + (5.2.plus(4.4)));
-  println("5.2 - 4.4 = " + (5.2.minus(4.4)));
-  println("5.2 * 4.4 = " + (5.2.multiply(4.4)));
-  println("5.2 / 4.4 = " + (5.2 / 4.4));
+    println """
+    ${new Date()}
 
-  // Order of operations
-  println("3 + 2 * 5 = " + (3 + 2 * 5));
-  println("(3 + 2) * 5 = " + ((3 + 2) * 5));
+    Dear $person,
 
-  // Increment and decrement
-  println("age++ = " + (age++));
-  println("++age = " + (++age));
-  println("age-- = " + (age--));
-  println("--age = " + (--age));
+    This is a Groovy Letter!
 
-  // Largest values
-  println("Biggest Int " + Integer.MAX_VALUE);
-  println("Smallest Int " + Integer.MIN_VALUE);
-
-  println("Biggest Float " + Float.MAX_VALUE);
-  println("Smallest Float " + Float.MIN_VALUE);
-
-  println("Biggest Double " + Double.MAX_VALUE);
-  println("Smallest Double " + Double.MIN_VALUE);
-
+    Sincerely,
+    Bob
+    """
+  }
 }
