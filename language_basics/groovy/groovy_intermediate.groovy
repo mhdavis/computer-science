10ac92cd3d169559726ca34cf66c4f040df9a13e
Michael Davis <mhdavis1993@gmail.com>
2018-03-19 11:52:05 -0400

    Added groovy section on OOP
    - Created groovy_class_examples w/ classes for OOP example

--- language_basics/groovy/groovy_intermediate.groovy
+++ language_basics/groovy/groovy_intermediate.groovy
@@ -195,4 +195,42 @@ class RangeExample {
   }
 }
 
-// ---------------- CLASSES + INHERITENCE -------------------
+// ---------------- OBJECT ORIENTED PROGRAMMING  -------------------
+/*
+In order to do OOP, some additional classes have been defined
+in the groovy_class_examples directory
+*/
+// Classes are blueprints that are used to define objects
+// Every object has attributes (fields) and capabilities
+// (methods)
+
+// Create an Animal object with named parameters
+// def king = new Animal(name : 'King', sound : 'Growl');
+// or with a Constructor
+class OOPExample {
+  static void main(String[] args) {
+    def king = new Animal('King', 'Growl');
+
+    println("${king.name} says ${king.sound}");
+
+    // Change an object attribute with a setter
+    // *** Groovy automatically creates a setter for you ***
+    king.setSound('Grrrr');
+    println("${king.name} says ${king.sound}");
+
+    king.run();
+
+    println(king.toString());
+
+    // With inheritance a class can inherit all fields
+    // and methods of another class
+    def grover = new Dog('Grover', 'Grrrrr', 'Derek');
+
+    king.makeSound();
+    grover.makeSound();
+
+    // Mammal inherits from the abstract class Thing
+    def hamster = new Mammal('Furry');
+    hamster.getInfo();
+  }
+}
